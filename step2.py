2f790489d0be616162339248dcf6a6c6ea8d3dc0
seyoung <airseyoung@naver.com>
2020-12-12 02:45:30 +0900

    Add : step-2 여러 문자 입력 구현
    여러 문자 입력받을 시 순서대로 처리하는 과정 추가

--- step2.py
+++ step2.py
@@ -2,7 +2,7 @@ arr = [['R', 'R', 'W'],
        ['G', 'C', 'W'],
        ['G', 'B', 'B']]
 
-
+# 큐브 상태 출력
 def print_cube():
     for i in arr:
         for j in i:
@@ -25,7 +25,6 @@ def relocation(n, num, loc):
             for j in range(len(arr[i])):
                 if i == idx:
                     arr[i][j] = transfer[j]
-
     print_cube()
 
 
@@ -47,49 +46,51 @@ def horizontal_transfer(num, idx):
     relocation(n, num, loc='horizontal')
 
 
+# 큐브 초기 상태
 print_cube()
-
 while True:
-    input_list = input('CUBE > ').split()
+    # 사용자 입력
+    input_str = input('CUBE > ')
     idx = 0
 
-    if input_list[0].find('\'') is -1:
-        num = 1
-        if input_list[0] == 'R' or 'B':
-            idx = 2
-            if input_list[0] == 'R':
-                vertical_transfer(int(num), int(idx))
-            # B의 경우 U와 방향이 반대
-            elif input_list[0] == 'B':
-                horizontal_transfer(-num, idx)
+    for i in input_str:
+        if i.find('\'') is -1:
+            num = 1
+            if i == 'R' or 'B':
+                idx = 2
+                if i == 'R':
+                    vertical_transfer(num, idx)
+                # B의 경우 U와 방향이 반대
+                elif i == 'B':
+                    horizontal_transfer(-num, idx)
 
-        if input_list[0] == 'L' or 'U':
-            idx = 0
-            if input_list[0] == 'L':
-                # L의 경우 R과 방향이 반대
-                vertical_transfer(int(-num), int(idx))
-            elif input_list[0] == 'U':
-                horizontal_transfer(num, idx)
+            if i == 'L' or 'U':
+                idx = 0
+                if i == 'L':
+                    # L의 경우 R과 방향이 반대
+                    vertical_transfer(-num, idx)
+                elif i == 'U':
+                    horizontal_transfer(num, idx)
 
-    else:
-        num = -1
-        if input_list[0] == 'R\'' or 'B\'':
-            idx = 2
-            if input_list[0] == 'R\'':
-                vertical_transfer(int(num), int(idx))
-            # B의 경우 U와 방향이 반대
-            elif input_list[0] == 'B\'':
-                horizontal_transfer(-num, idx)
+        else:
+            num = -1
+            if i == 'R\'' or 'B\'':
+                idx = 2
+                if i == 'R\'':
+                    vertical_transfer(num, idx)
+                # B의 경우 U와 방향이 반대
+                elif i == 'B\'':
+                    horizontal_transfer(-num, idx)
 
-        if input_list[0] == 'L\'' or 'U\'':
-            idx = 0
-            if input_list[0] == 'L\'':
-                # L의 경우 R과 방향이 반대
-                vertical_transfer(int(-num), int(idx))
-            elif input_list[0] == 'U\'':
-                horizontal_transfer(num, idx)
+            if i == 'L\'' or 'U\'':
+                idx = 0
+                if i == 'L\'':
+                    # L의 경우 R과 방향이 반대
+                    vertical_transfer(-num, idx)
+                elif i == 'U\'':
+                    horizontal_transfer(num, idx)
 
     # 종료
-    if input_list[0] == 'Q':
+    if input_str[0] == 'Q':
         print('Bye~')
         break
