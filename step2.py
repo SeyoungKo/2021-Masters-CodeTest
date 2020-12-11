c795b69bbd8a979b06ef50ba72d36d82a179b563
seyoung <airseyoung@naver.com>
2020-12-12 02:08:29 +0900

    Add : step-2 가로 큐브 이동 구현
    문자 하나 입력시 가로 큐브 위/아래로 이동하는 코드

--- step2.py
+++ step2.py
@@ -1,91 +1,95 @@
-# U, U' - 가장 윗줄을 왼쪽/오른쪽 이동
-# R, R' - 가장 오른쪽(세로)를 위/아래로 이동
-# L, L' - 가장 왼쪽(세로)를 위/아래로 이동
-# B, B' - 가장 아랫줄을 왼쪽/오른쪽 이동
-
 arr = [['R', 'R', 'W'],
        ['G', 'C', 'W'],
        ['G', 'B', 'B']]
+
 
 def print_cube():
     for i in arr:
         for j in i:
             print(j, end=" ")
         print()
+    print(' ')
 
-# 큐브 이동
-def cube_transfer(num, idx):
+
+# 이동한 큐브 재배치
+def relocation(n, num, loc):
+    transfer = n[num:] + n[:num]
+    if loc == 'vertical':
+        for i in range(len(arr)):
+            for j in range(len(arr[i])):
+                if j == idx:
+                    arr[i][j] = transfer[i]
+
+    elif loc == 'horizontal':
+        for i in range(len(arr)):
+            for j in range(len(arr[i])):
+                if i == idx:
+                    arr[i][j] = transfer[j]
+
+    print_cube()
+
+
+# 위/아래 큐브 이동
+def vertical_transfer(num, idx):
     n = []
     for i in arr:
         n.append(i[idx])
-    transfer = n[num:] + n[:num]
+    relocation(n, num, loc='vertical')
 
+
+# 오/왼 큐브 이동
+def horizontal_transfer(num, idx):
+    n = []
     for i in range(len(arr)):
-        for j in range(len(arr[i])):
-            if j == idx:
-                arr[i][j] = transfer[i]
-    print_cube()
+        if i == idx:
+            for j in range(len(arr[i])):
+                n.append(arr[i][j])
+    relocation(n, num, loc='horizontal')
 
-def move_vertical(move, idx):
-    # 위로 이동
-    if move.find('\'') is -1:
-        num = -1
-    # 아래로 이동
-    else:
-        num = 1
-    cube_transfer(int(num), int(idx))
-
-    # if move == 'R':
-    #
-    #     for i in arr:
-    #         n.append(i[2])
-    #     transfer = n[-1:] + n[:-1]
-    #
-    #     for i in range(len(arr)):
-    #         for j in range(len(arr[i])):
-    #             if j ==2:
-    #                 arr[i][j] = transfer[i]
-    #
-    # elif move == 'R\'':
-    #     for i in arr:
-    #         n.append(i[2])
-    #     transfer = n[1:] + n[:1]
-    #
-    #     for i in range(len(arr)):
-    #         for j in range(len(arr[i])):
-    #             if j ==2:
-    #                 arr[i][j] = transfer[i]
-    #
-    # print_cube()
-
-# def move_horizontal(move, index):
 
 print_cube()
-print(' ')
+
 while True:
     input_list = input('CUBE > ').split()
-    idx =0
+    idx = 0
 
-    # 세로 이동
-    if input_list[0] == 'R' or 'R\'':
-        idx = 2
+    if input_list[0].find('\'') is -1:
+        num = 1
+        if input_list[0] == 'R' or 'B':
+            idx = 2
+            if input_list[0] == 'R':
+                vertical_transfer(int(num), int(idx))
+            # B의 경우 U와 방향이 반대
+            elif input_list[0] == 'B':
+                horizontal_transfer(-num, idx)
 
-    elif input_list[0] == 'L' or 'L\'':
-        idx = 0
-    move_vertical(input_list[0], idx)
+        if input_list[0] == 'L' or 'U':
+            idx = 0
+            if input_list[0] == 'L':
+                # L의 경우 R과 방향이 반대
+                vertical_transfer(int(-num), int(idx))
+            elif input_list[0] == 'U':
+                horizontal_transfer(num, idx)
 
-    # 가로 이동
-    if input_list[0] == 'U' or 'U\'':
-        idx = 0
-        # move_horizontal(input_list[0], idx)
+    else:
+        num = -1
+        if input_list[0] == 'R\'' or 'B\'':
+            idx = 2
+            if input_list[0] == 'R\'':
+                vertical_transfer(int(num), int(idx))
+            # B의 경우 U와 방향이 반대
+            elif input_list[0] == 'B\'':
+                horizontal_transfer(-num, idx)
 
-    elif input_list[0] == 'B' or 'B\'':
-        idx = 2
-        # move_horizontal(input_list[0], idx)
+        if input_list[0] == 'L\'' or 'U\'':
+            idx = 0
+            if input_list[0] == 'L\'':
+                # L의 경우 R과 방향이 반대
+                vertical_transfer(int(-num), int(idx))
+            elif input_list[0] == 'U\'':
+                horizontal_transfer(num, idx)
 
     # 종료
     if input_list[0] == 'Q':
         print('Bye~')
         break
-
-
